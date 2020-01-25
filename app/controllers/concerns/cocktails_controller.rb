72ad4db630da37deb4eacdcd8a1cefc56c6b3337
Tom Hudson <tom.hudson89@gmail.com>
2020-01-25 16:56:12 +0000

    done 1-5

---
+++ app/controllers/concerns/cocktails_controller.rb
@@ -0,0 +1,31 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:cocktail).permit(:name)
+  end
+end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,4 @@ class Cocktail < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-
 end
